44f9b859e6ed936dc6e0c0b0183488b7839583b1
jieunGo <ggozi@m4a.co.kr>
2023-01-04 15:47:30 +0900

    -

--- js/common.js
+++ js/common.js
@@ -1,12 +1,24 @@
 $(function(){
-
+	swiperTotal.init();
 });
 
 const swiperTotal = {
-    init: function() {
-        
-    },
-    lySwiper : function() {
+	init: function() {
+		swiperTotal.lySwiper();
+	},
+	lySwiper : function(e) {
+		let swiperMain = document.querySelector('.ly-swiper');
 
-    }
+		swiperMain.forEach((el,idx) => {
+			el.classList.add(''+ idx);
+
+			
+			return new Swiper(this, {
+				direction: 'vertical',
+				loop: false,
+				a11y: false,
+			})
+		})
+
+	}
 }
--- js/head.js
+++ js/head.js
@@ -1,6 +1,7 @@
 (function () {
   let str = '';
   str += '<link rel="stylesheet" href="../css/font.css" />';
+  str += '<link rel="stylesheet" href="../css/lib/swiper-bundle.min.css" />';
   str += '<link rel="stylesheet" href="../css/style.css" />';
   str += '<script type="text/javascript" src="../js/lib/jquery.min.js"></script>';
   str += '<script type="text/javascript" src="../js/lib/swiper-bundle.min.js"></script>';
