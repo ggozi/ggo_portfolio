0424e3f9c79e4d605098930a68e75c3bd33dde8f
soominseop <soominimus@gmail.com>
2023-04-05 17:32:32 +0900

    23.04.15 수정

--- js/common.js
+++ js/common.js
@@ -1,6 +1,52 @@
 $(function(){
-	swiperTotal.init();
+	//swiperTotal.init();
+	uiHtml.include();
+
+	
+	// var swiper = new Swiper(".mySwiper", {
+	// 	spaceBetween: 50,
+	// 	pagination: {
+	// 	el: ".swiper-pagination",
+	// 	clickable: true,
+	// 	},
+	// });
+	// var swiper2 = new Swiper(".mySwiper2", {
+	// 	direction: "vertical",
+	// 	spaceBetween: 50,
+	// 	pagination: {
+	// 	el: ".swiper-pagination",
+	// 	clickable: true,
+	// 	autoHeight: false
+	// 	},
+	// });
+
+	var controller = new ScrollMagic.Controller();
+	var wipeAnimation = new TimelineMax()
+	// animate to second
+	.to("#slideContainer", 1, {z: -180} )
+	.to("#slideContainer", 1, {x:"-25%"} )
+	.to("#slideContainer", 1, {z: 0} )
+	// animate to third
+	.to("#slideContainer", 1, {z: -180, delay: 0.6} )
+	.to("#slideContainer", 1, {x:"-50%"} )
+	.to("#slideContainer", 1, {z: 0} )
+	// animate to forth
+	.to("#slideContainer", 1, {z: -180, delay: 0.6} )
+	.to("#slideContainer", 1, {x:"-75%"} )
+	.to("#slideContainer", 1, {z: 0} )
+
+	var scene = new ScrollMagic.Scene({
+		triggerElement: "#pinContainer",
+		triggerHook: "onLeave",
+		duration: "300%" //이 값이 클 수록 천천히 덮어씀
+	})
+	.setPin("#pinContainer")
+	.setTween(wipeAnimation)
+	.addIndicators() 
+	.addTo(controller);
+
 });
+
 
 const swiperTotal = {
 	init: function() {
@@ -9,16 +55,54 @@ const swiperTotal = {
 	lySwiper : function(e) {
 		let swiperMain = document.querySelector('.ly-swiper');
 
-		swiperMain.forEach((el,idx) => {
-			el.classList.add(''+ idx);
+
+		new Swiper(swiperMain, {
+			direction: 'vertical',
+			loop: false,
+			a11y: false,
+		})
+		
+		// swiperMain.forEach((el,idx) => {
+		// 	//el.classList.add(''+ idx);
 
 			
-			return new Swiper(this, {
-				direction: 'vertical',
-				loop: false,
-				a11y: false,
-			})
-		})
+		// 	return 
+		// })
 
 	}
 }
+
+//Html include
+const uiHtml = {
+	include: function (fn) {
+		console.log(1)
+	  const $elements = $.find('*[data-include]');
+	  if ($elements.length) {
+		if (location.host) {
+		  $.each($elements, function (i) {
+			const $this = $(this);
+			$this.empty();
+			const $html = $this.data('include');
+			const $htmlAry = $html.split('/');
+			const $htmlFile = $htmlAry[$htmlAry.length - 1];
+			const $docTitle = document.title;
+			let $title = null;
+			if ($docTitle.indexOf(' | ') > -1) {
+			  $title = $docTitle.split(' | ')[0];
+			}
+			$this.load($html, function (sta) {
+			  if (sta == 'success') {
+				if (!$this.attr('class') && !$this.attr('id')) $this.children().unwrap();
+				else $this.removeAttr('data-include');
+			  }
+			  if (i === $elements.length - 1) {
+				if (!!fn) fn();
+			  }
+			});
+		  });
+		} else {
+		  if (!!fn) fn();
+		}
+	  }
+	}
+  };
--- js/head.js
+++ js/head.js
@@ -1,12 +1,16 @@
 (function () {
-  let str = '';
-  str += '<link rel="stylesheet" href="../css/font.css" />';
-  str += '<link rel="stylesheet" href="../css/lib/swiper-bundle.min.css" />';
-  str += '<link rel="stylesheet" href="../css/style.css" />';
-  str += '<script type="text/javascript" src="../js/lib/jquery.min.js"></script>';
-  str += '<script type="text/javascript" src="../js/lib/swiper-bundle.min.js"></script>';
-  str += '<script type="text/javascript" src="../js/common.js"></script>';
-  document.write(str);
-  const $include = document.querySelector('.__include');
-  if ($include) $include.remove();
+	let str = '';
+	str += '<link rel="stylesheet" href="../css/font.css" />';
+	str += '<link rel="stylesheet" href="../css/lib/swiper-bundle.min.css" />';
+	str += '<link rel="stylesheet" href="../css/style.min.css" />';
+	str += '<script type="text/javascript" src="../js/lib/jquery.min.js"></script>';
+	// str += '<script type="text/javascript" src="../js/lib/swiper-bundle.min.js"></script>';
+	str += '<script type="text/javascript" src="../js/lib/scrolling/ScrollMagic.min.js"></script>';
+	str += '<script type="text/javascript" src="../js/lib/scrolling/debug.addIndicators.min.js"></script>';
+	str += '<script type="text/javascript" src="../js/lib/scrolling/TweenMax.min.js"></script>';
+	str += '<script type="text/javascript" src="../js/lib/scrolling/animation.gsap.js"></script>';
+	str += '<script type="text/javascript" src="../js/common.js"></script>';
+	document.write(str);
+	const $include = document.querySelector('.__include');
+	if ($include) $include.remove();
 })();
